50526ce7f4bcfa67389bfa09de9a7f14d3d9181b
June Youn <youngyojun@gmail.com>
2025-06-26 19:18:19 +0900

    Upload mp3 files

--- Lectures/Audio/converter.py
+++ Lectures/Audio/converter.py
@@ -2,10 +2,17 @@ import os, sys
 
 files = os.listdir()
 
+# for filename in files:
+# 	if ".wma" == filename[-4:]:
+# 		filename = filename[:-4]
+# 
+# 		os.system("ffmpeg -y -i {0}.wma {0}.wav".format(filename))
+# 		os.system("ffmpeg -y -i {0}.wav -af 'volume=30' {0}-large.wav".format(filename))
+# 		os.system("mv {0}-large.wav {0}.wav".format(filename))
+
 for filename in files:
-	if ".wma" == filename[-4:]:
+	if ".wav" == filename[-4:]:
 		filename = filename[:-4]
 
-		os.system("ffmpeg -y -i {0}.wma {0}.wav".format(filename))
-		os.system("ffmpeg -y -i {0}.wav -af 'volume=30' {0}-large.wav".format(filename))
-		os.system("mv {0}-large.wav {0}.wav".format(filename))
+		os.system("ffmpeg -i {0}.wav -b:a 128k {0}.mp3".format(filename))
+
